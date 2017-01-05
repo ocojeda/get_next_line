d4c14e8c765812af494be95fd94df4fea79fbe32
Mathilde YERNAUX <myernaux@e2r13p4.42.fr>
2017-01-05 12:37:36 +0100

    POLLOOOOOOOOOOOOOOOOOOOON

--- get_next_line.c
+++ get_next_line.c
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ocojeda- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: myernaux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/05 10:31:28 by ocojeda-          #+#    #+#             */
-/*   Updated: 2017/01/04 15:20:28 by ocojeda-         ###   ########.fr       */
+/*   Created: 2016/12/13 14:24:46 by myernaux          #+#    #+#             */
+/*   Updated: 2017/01/05 12:37:08 by myernaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
 #include "get_next_line.h"
 
-int	read_to_buff(int fd, char **string)
+static int			read_to_buff(int fd, char **string)
 {
 	char	buff[BUFF_SIZE + 1] = {'\n'};
 	int		ret;
@@ -23,40 +22,39 @@ int	read_to_buff(int fd, char **string)
 	if (ret > 0)
 	{
 		new_string = ft_strjoin(*string, buff);
-		if(!new_string)
-			return -1;
+		if (!new_string)
+			return (-1);
 		free(*string);
 		*string = new_string;
 	}
-	return ret;
+	return (ret);
 }
-int get_next_line(const int fd, char **line)
+
+int					get_next_line(const int fd, char **line)
 {
 	static char *buff = NULL;
-	int ret;
-	char *index;
+	int			ret;
+	char		*index;
 
-	if(!buff && (buff = (char *)ft_memalloc(sizeof(char))) == NULL)
-		return -1;
+	if (!buff && (buff = (char *)ft_memalloc(sizeof(char))) == NULL)
+		return (-1);
 	index = ft_strchr(buff, '\n');
-	while(index == NULL)
+	while (index == NULL)
 	{
-		ret = read_to_buff(fd, &buff);
-		if (ret == 0)
+		if ((ret = read_to_buff(fd, &buff)) == 0)
 		{
-			if((index = ft_strchr(buff, '\0')) == buff)
-				return 0;
+			if ((index = ft_strchr(buff, '\0')) == buff)
+				return (0);
 		}
-		else if(ret < 0)
-			return -1;
-		else 
+		else if ((ret = read_to_buff(fd, &buff)) < 0)
+			return (-1);
+		else
 			index = ft_strchr(buff, '\n');
 	}
-	*line = ft_strsub(buff, 0 , index - buff);
 	*index = '\0';
-	if(!*line)
-		return -1;
+	if (!(*line = ft_strsub(buff, 0, index - buff)))
+		return (-1);
 	free(buff);
 	buff = ft_strdup(++index);
-	return 1;
+	return (1);
 }
