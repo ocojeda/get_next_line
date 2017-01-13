9db2e4b238ddb6a1d59c3eca0ef8bc8fc0642f10
Octavio orlando OJEDA ARONIZ <ocojeda-@e2r13p2.42.fr>
2017-01-13 15:18:00 +0100

    test fd

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,34 @@
 /*   By: myernaux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 14:24:46 by myernaux          #+#    #+#             */
-/*   Updated: 2017/01/13 13:05:10 by ocojeda-         ###   ########.fr       */
+/*   Updated: 2017/01/13 15:11:24 by ocojeda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft/libft.h"
+
+t_gnl	*check_fd(int fd, t_gnl *first)
+{
+	t_gnl *temp;
+	t_gnl *temp2;
+
+	temp = first;
+	while(temp)
+	{
+		if(fd == temp->fd)
+		{
+			return temp;
+		}
+		temp = temp->next;
+	}
+	temp = temp2;
+	temp->next = NULL;
+	if(!temp->buff && (temp->buff = (char *)ft_memalloc(sizeof(char))) == NULL)
+			return (NULL);
+	temp->fd = fd;
+	return (temp);
+}
 
 static int			read_to_buff(int fd, char **string)
 {
@@ -19,7 +41,9 @@ static int			read_to_buff(int fd, char **string)
 	int		ret;
 	char	*new_string;
 
+	
 	buff = ft_strnew(BUFF_SIZE + 1);
+	ft_memset(buff, '\n', sizeof(buff));
 	ret = read(fd, buff, BUFF_SIZE);
 	if (ret > 0)
 	{
@@ -30,35 +54,35 @@ static int			read_to_buff(int fd, char **string)
 		free(*string);
 		*string = new_string;
 	}
-	free(buff);	
+	free(buff);
 	return (ret);
 }
 
 int					get_next_line(const int fd, char **line)
 {
-	static char *buff = NULL;
+	static t_gnl *all = NULL;
+	t_gnl *current;
 	int			ret;
 	char		*index;
 
-	if (!buff && (buff = (char *)ft_memalloc(sizeof(char))) == NULL)
-		return (-1);
-	index = ft_strchr(buff, '\n');
+	current = check_fd(fd, all);
+	index = ft_strchr(current->buff, '\n');
 	while (index == NULL)
 	{
-		if ((ret = read_to_buff(fd, &buff)) == 0)
+		if ((ret = read_to_buff(current->fd, &current->buff)) == 0)
 		{
-			if ((index = ft_strchr(buff, '\0')) == buff)
+			if ((index = ft_strchr(current->buff, '\0')) == current->buff)
 				return (0);
 		}
 		else if (ret < 0)
 			return (-1);
 		else
-			index = ft_strchr(buff, '\n');
+			index = ft_strchr(current->buff, '\n');
 	}
-	if (!(*line = ft_strsub(buff, 0, index - buff)))
+	if (!(*line = ft_strsub(current->buff, 0, index - current->buff)))
 		return (-1);
 	index = ft_strdup(index + 1);
-	free(buff);
-	buff = index;
+	free(current->buff);
+	current->buff = index;
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,18 @@
 /*   By: ocojeda- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/27 17:09:34 by ocojeda-          #+#    #+#             */
-/*   Updated: 2017/01/07 15:19:31 by ocojeda-         ###   ########.fr       */
+/*   Updated: 2017/01/13 13:27:56 by ocojeda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 8
-
+typedef struct s_gnl
+{
+	int fd;
+	char *buff;
+	struct s_gnl *next;
+}				t_gnl;
 int get_next_line(const int fd, char **line);
 #endif
