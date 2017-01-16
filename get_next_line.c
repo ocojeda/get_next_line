50a73051b9857c82b443bd5e35be375a6cbf74fa
Octavio orlando OJEDA ARONIZ <ocojeda-@e2r13p2.42.fr>
2017-01-16 18:01:30 +0100

    merde alors

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: myernaux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 14:24:46 by myernaux          #+#    #+#             */
-/*   Updated: 2017/01/16 11:52:50 by ocojeda-         ###   ########.fr       */
+/*   Updated: 2017/01/16 17:59:05 by ocojeda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@
 static t_list			*check_fd(t_list **file, int fd)
 {
 	t_list				*tmp;
+	t_list				*temp2;
 
 	tmp = *file;
 	while (tmp)
 	{
 		if ((int)tmp->content_size == fd)
 			return (tmp);
+		temp2 = tmp;
 		tmp = tmp->next;
 	}
 	tmp = ft_lstnew("\0", fd);
@@ -34,11 +36,9 @@ static int			read_to_buff(t_list *current)
 	char	*buff;
 	int		ret;
 	char	*new_string;
-	int fd = current->content_size;
 
 	buff = ft_strnew(BUFF_SIZE + 1);
-	ft_memset(buff, '\n', sizeof(buff));
-	ret = read(fd, buff, BUFF_SIZE);
+	ret = read(current->content_size, buff, BUFF_SIZE);
 	if (ret > 0)
 	{
 		buff[ret] = '\0';
@@ -46,38 +46,36 @@ static int			read_to_buff(t_list *current)
 		if (!new_string)
 			return (-1);
 		free(current->content);
-		current->content = ft_strdup(new_string);
+		current->content = new_string;
 	}
-	free(buff);
+	free(buff);	
 	return (ret);
 }
-
 int					get_next_line(const int fd, char **line)
 {
-	static t_list *all;
-	t_list *current;
 	int			ret;
-	char		*index;
+	static t_list		*all;
+	t_list	*curr;
+	char *index;
 
-	current = check_fd(&all, fd);
-	index = ft_strchr(current->content, '\n');
+	curr= check_fd(&all, fd);
+	index = ft_strchr((char *)curr->content, '\n');
 	while (index == NULL)
 	{
-		ret = read_to_buff(current);
-		if (ret == 0)
+		if ((ret = read_to_buff(curr) == 0))
 		{
-			if ((index = ft_strchr(current->content, '\0')) == current->content)
+			if ((index = ft_strchr(curr->content, '\0')) == curr->content)
 				return (0);
 		}
 		else if (ret < 0)
 			return (-1);
 		else
-			index = ft_strchr(current->content, '\n');
+			index = ft_strchr(curr->content, '\n');
 	}
-	if (!(*line = ft_strsub(current->content, 0, index - (char *)current->content)))
+	if (!(*line = ft_strsub(curr->content, 0, index - (char *)curr->content)))
 		return (-1);
 	index = ft_strdup(index + 1);
-	free(current->content);
-	current->content = ft_strdup(index);
+	free(curr->content);
+	curr->content = index;
 	return (1);
 }
